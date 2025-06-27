c7ef3d682df54d9b03e49b0ebe3a59f292640aea
karina-krupodior <karinakrupodior@gmail.com>
2025-06-26 20:00:01 -0500

    added getCart

--- capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,17 +1,29 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
+import java.util.List;
 
 // convert this class to a REST controller
+@RestController
+@RequestMapping("/cart")
 // only logged in users should have access to these actions
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+@CrossOrigin
 public class ShoppingCartController
 {
     // a shopping cart requires
@@ -20,7 +32,15 @@ public class ShoppingCartController
     private ProductDao productDao;
 
 
+@Autowired
+    public ShoppingCartController (ShoppingCartDao shoppingCartDao,UserDao userDao,ProductDao productDao ) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
 
+    }
+    // GET
+    @GetMapping("")
     // each method in this controller requires a Principal object as a parameter
     public ShoppingCart getCart(Principal principal)
     {
@@ -33,7 +53,14 @@ public class ShoppingCartController
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+
+            List<ShoppingCartItem> items = (List<ShoppingCartItem>) shoppingCartDao.getByUserId(userId);
+          ShoppingCart  cart = new ShoppingCart();
+          for (ShoppingCartItem item : items) {
+              cart.add(item);
+
+          }
+          return  cart;
         }
         catch(Exception e)
         {
