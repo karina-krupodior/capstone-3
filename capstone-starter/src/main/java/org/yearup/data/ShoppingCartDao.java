69d6d32eadc89cb2e2294ce09781b892fc3c73cc
karina-krupodior <karinakrupodior@gmail.com>
2025-06-26 21:11:02 -0500

    added ShoppingCartDao

--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -2,8 +2,14 @@ package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
 
+
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+
+    void addProductToCart(int userId, int productId);
+
+    void updateProductQuantity(int userId, int productId, int quantity);
+
+    void clearCart(int userId);
 }
