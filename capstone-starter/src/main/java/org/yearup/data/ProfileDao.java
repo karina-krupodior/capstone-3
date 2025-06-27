dde7b274ce520ec78b6362a6b9dcf04d5a1dd4fe
karina-krupodior <karinakrupodior@gmail.com>
2025-06-26 21:48:03 -0500

    Implemented user profile GET and PUT endpoints

--- capstone-starter/src/main/java/org/yearup/data/ProfileDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,11 @@
-package org.yearup.data;
 
+package org.yearup.data;
 
 import org.yearup.models.Profile;
 
 public interface ProfileDao
 {
     Profile create(Profile profile);
+    Profile getByUserId(int userId);
+    void update(Profile profile);
 }
--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -1,8 +1,8 @@
 package org.yearup.data.mysql;
 
 import org.springframework.stereotype.Component;
-import org.yearup.models.Profile;
 import org.yearup.data.ProfileDao;
+import org.yearup.models.Profile;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -19,11 +19,11 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
     public Profile create(Profile profile)
     {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
+        try (Connection connection = getConnection())
         {
-            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = connection.prepareStatement(sql);
             ps.setInt(1, profile.getUserId());
             ps.setString(2, profile.getFirstName());
             ps.setString(3, profile.getLastName());
@@ -44,4 +44,80 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         }
     }
 
+    //  getByUserId
+    @Override
+    public Profile getByUserId(int userId)
+    {
+        String sql = "SELECT * FROM profiles WHERE user_id = ?";
+
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, userId);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+            {
+                return mapRow(rs);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    // update
+    @Override
+    public void update(Profile profile)
+    {
+        String sql = "UPDATE profiles SET " +
+                "first_name = ?, " +
+                "last_name = ?, " +
+                "phone = ?, " +
+                "email = ?, " +
+                "address = ?, " +
+                "city = ?, " +
+                "state = ?, " +
+                "zip = ? " +
+                "WHERE user_id = ?";
+
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, profile.getFirstName());
+            ps.setString(2, profile.getLastName());
+            ps.setString(3, profile.getPhone());
+            ps.setString(4, profile.getEmail());
+            ps.setString(5, profile.getAddress());
+            ps.setString(6, profile.getCity());
+            ps.setString(7, profile.getState());
+            ps.setString(8, profile.getZip());
+            ps.setInt(9, profile.getUserId());
+
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private Profile mapRow(ResultSet row) throws SQLException
+    {
+        int userId = row.getInt("user_id");
+        String firstName = row.getString("first_name");
+        String lastName = row.getString("last_name");
+        String phone = row.getString("phone");
+        String email = row.getString("email");
+        String address = row.getString("address");
+        String city = row.getString("city");
+        String state = row.getString("state");
+        String zip = row.getString("zip");
+
+        return new Profile(userId, firstName, lastName, phone, email, address, city, state, zip);
+    }
 }
